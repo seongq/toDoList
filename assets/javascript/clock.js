22d41476ea6fe4e34f157dbb42cabddc2d8d8cee
seongq <36037178+seongq@users.noreply.github.com>
2020-06-25 17:36:23 +0900

    input box moving issue solved
    한글 입력시 인풋박스가 올라가는 현상을 바로잡음

--- assets/javascript/clock.js
+++ assets/javascript/clock.js
@@ -1,5 +1,10 @@
+const clock = document.querySelector(".clock");
 
-function init(){
-    
+function init() {
+  //   첫화면에서 시간 출력
+  //   loadingTime();
+  // 시간을 지속해서 보여주기
+  //   showTime();
 }
-init{}
+
+init();
